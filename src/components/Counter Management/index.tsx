b17547140e6bb60edfaf9be1d1f1596847854328
Sean Phillips <seanphillips45@gmail.com>
2021-09-29 21:49:45 -0400

    State!

--- src/components/Counter Management/index.tsx
+++ src/components/Counter Management/index.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import {CounterManagementProps, CounterManagementState } from './interface';
 
 class CounterManagement extends React.Component<CounterManagementProps, CounterManagementState> {
+    constructor(props: CounterManagementProps) {
+        super(props);
+
+        this.state = {
+            counter: 0
+        }
+    }
+
+handleAddClick = () => {
+    this.setState({ counter: this.state.counter + 1 })
+}
+
+handleSubClick = () => {
+    this.setState({ counter: this.state.counter - 1 })
+}
+
     render() {
         const { ownerName } = this.props;
+        const { counter } = this.state;
         return (
             <div>
-                <h1>
-                    Counter Management
-                    <h2>Owner Name: {ownerName}</h2>
-                </h1>
+                <h1>Counter Management</h1>
+                <h2>Owner Name: {ownerName}</h2>
+                <h2>Counter: {counter}</h2>
+                <button onClick={this.handleAddClick}>Add</button>
+                <button onClick={this.handleSubClick}>Subtract</button>
             </div>
         )
     }
